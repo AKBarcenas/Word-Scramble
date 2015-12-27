2f90e881e0a6fd00bf6388b3e5b1d924c44cbef2
AKBarcenas <akbarcen@ucsd.edu>
2015-12-27 11:00:46 -0800

    added error messages

--- Word Scramble/MasterViewController.swift
+++ Word Scramble/MasterViewController.swift
@@ -87,6 +87,9 @@ class MasterViewController: UITableViewController {
     func submitAnswer(answer: String)  {
         let lowerAnswer = answer.lowercaseString
         
+        let errorTitle: String
+        let errorMessage: String
+        
         if wordIsPossible(lowerAnswer) {
             if wordIsOriginal(lowerAnswer) {
                 if wordIsReal(lowerAnswer) {
@@ -94,21 +97,59 @@ class MasterViewController: UITableViewController {
                     
                     let indexPath = NSIndexPath(forRow: 0, inSection: 0)
                     tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+                    
+                    return
+                }
+                else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
             }
+            
+            else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+            }
         }
+        
+        else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from '\(title!.lowercaseString)'!"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
     
+
+    
     func wordIsPossible(word: String) -> Bool {
+        var tempWord = title!.lowercaseString
+        
+        for letter in word.characters {
+            if let pos = tempWord.rangeOfString(String(letter)) {
+                tempWord.removeAtIndex(pos.startIndex)
+            }
+            
+            else {
+                return false
+            }
+        }
+        
         return true
     }
     
     func wordIsOriginal(word: String) -> Bool {
-        return true
+        return !objects.contains(word)
     }
     
     func wordIsReal(word: String) -> Bool {
-        return true
+        let checker = UITextChecker()
+        let range = NSMakeRange(0, word.characters.count)
+        let misspelledRange = checker.rangeOfMisspelledWordInString(word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
 
 }
