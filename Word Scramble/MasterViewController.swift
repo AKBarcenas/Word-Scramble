92162773cd382a62f399883e858b42c2e54d41ce
AKBarcenas <akbarcen@ucsd.edu>
2015-12-27 01:06:52 -0800

    Added random word generation

--- Word Scramble/MasterViewController.swift
+++ Word Scramble/MasterViewController.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import GameplayKit
 
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var objects = [String]()
+    var allWords = [String]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+            }
+        }
+        
+        else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -42,9 +56,16 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
     }
 
 }
